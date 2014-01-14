1a1921898dbe149293fd592cb134f5b09feb9b92
Hagen Schink <h.schink@finn.de>
2014-01-14 08:35:03 +0100

    Adds helper method for menu item locale

--- app/helpers/accessibility_helper.rb
+++ app/helpers/accessibility_helper.rb
@@ -36,4 +36,33 @@ module AccessibilityHelper
   def empty_element_tag
     @empty_element_tag ||= ApplicationController.new.render_to_string(partial: "accessibility/empty_element_tag").html_safe
   end
+
+  # Returns the locale :en for the given menu item if the user locale is
+  # different but equals the English translation
+  #
+  # Returns nil if user locale is :en (English) or no translation is given,
+  # thus, assumes English to be the default language. Returns :en iff a
+  # translation exists and the translation equals the English one.
+  def menu_item_locale(menu_item)
+    return nil if english_locale_set?
+
+    caption_content = menu_item.instance_variable_get(:@caption)
+    locale_label = caption_content.is_a?(Symbol) ? caption_content : :"label_#{menu_item.name.to_s}"
+
+    (!locale_exists?(locale_label) || equals_english_locale(locale_label)) ? :en : nil
+  end
+
+  private
+
+  def locale_exists?(key, locale=I18n.locale)
+    I18n.t(key, locale: locale, raise: true) rescue false
+  end
+
+  def english_locale_set?
+    I18n.locale == :en
+  end
+
+  def equals_english_locale(key)
+    key.is_a?(Symbol) ? (I18n.t(key) == I18n.t(key, locale: :en)) : false
+  end
 end
